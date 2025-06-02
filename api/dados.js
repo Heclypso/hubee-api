754f8df9bb6bbbca9d25a36dc0cb1115622038b0
Heclypso <heclypso@gmail.com>
2025-06-02 16:09:42 -0300

    Commit inicial

---
+++ api/dados.js
@@ -0,0 +1,111 @@
+export const moveisUsados = [
+    {
+        title: 'Sofá reclinável 3 lugares',
+        images: ['https://servidor-estatico-alpha-six.vercel.app/sofa-tres-lugares-frente.jpg', 'https://servidor-estatico-alpha-six.vercel.app/sofa-tres-lugares-lateral.jpg', 'https://servidor-estatico-alpha-six.vercel.app/sofa-tres-lugares-costas.jpg'],
+        tags: ['Semi-novo'],
+        price: '1x de R$ 157,00',
+        installment: 'Em até 3x sem juros de R$ 52,33 ',
+    },
+    {
+        title: 'Sofá reclinável 2 lugares',
+        images: ['https://servidor-estatico-alpha-six.vercel.app/sofa-dois-lugares-frente.jpg', 'https://servidor-estatico-alpha-six.vercel.app/sofa-dois-lugares-lateral.jpg'],
+        tags: ['Semi-novo'],
+        price: '1x de R$ 157,00',
+        installment: 'Em até 3x sem juros de R$ 52,33 ',
+    },
+    {
+        title: 'Gaiola de coelho',
+        images: ['https://servidor-estatico-alpha-six.vercel.app/gaiola-de-coelho.jpg'],
+        tags: ['Semi-novo'],
+        price: '1x de R$ 157,00',
+        installment: 'Em até 3x sem juros de R$ 52,33 ',
+    },
+    {
+        title: 'Rack de sala',
+        images: ['https://servidor-estatico-alpha-six.vercel.app/rack-de-sala.jpg'],
+        tags: ['Semi-novo'],
+        price: '1x de R$ 157,00',
+        installment: 'Em até 3x sem juros de R$ 52,33 ',
+    }
+]
+
+export const consertoDeRoupas = [
+    {
+        title: 'Placeholder',
+        images: ['url'],
+        tags: ['P/Serviço'],
+        price: '1x de R$ 157,00',
+    },
+    {
+        title: 'Placeholder',
+        images: ['url'],
+        tags: ['P/Serviço'],
+        price: '1x de R$ 157,00',
+    },
+]
+
+export const confeitaria = [
+    {
+        title: 'Placeholder',
+        images: ['url'],
+        tags: ['P/Kg'],
+        price: '1x de R$ 157,00',
+    },
+        {
+        title: 'Placeholder',
+        images: ['url'],
+        tags: ['P/Unidade'],
+        price: '1x de R$ 157,00',
+    },
+        {
+        title: 'Placeholder',
+        images: ['url'],
+        tags: ['P/Cento'],
+        price: '1x de R$ 157,00',
+    },
+]
+
+export const outrosServiços = [
+    {
+        title: 'Acompanhante hospitalar',
+        images: ['url'],
+        tags: ['Diária', 'P/Hora', 'Cubro folga'],
+        price: '1x de R$ 157,00',
+    },
+    {
+        title: 'Cuidadora de idoso',
+        images: ['url'],
+        tags: ['Diária', 'P/Hora', 'Cubro folga'],
+        price: '1x de R$ 157,00',
+    },    
+    {
+        title: 'Pet sitter, cuidadora de animais',
+        images: ['url'],
+        tags: ['Diária', 'P/Hora'],
+        price: '1x de R$ 157,00',
+    },
+    {
+        title: 'Assistente de vida domiciliar ',
+        images: ['url'],
+        tags: ['Diária', 'P/Hora', 'Cubro folga'],
+        price: '1x de R$ 157,00',
+    },
+    {
+        title: 'Babá',
+        images: ['url'],
+        tags: ['Diária', 'P/Hora', 'Cubro folga'],
+        price: '1x de R$ 157,00',
+    },
+    {
+        title: 'Organizadora profissional',
+        images: ['url'],
+        tags: ['Diária', 'P/Hora', 'Cubro folga'],
+        price: '1x de R$ 157,00',
+    },
+    {
+        title: 'Monitora de festa infantil ',
+        images: ['url'],
+        tags: ['P/Serviço'],
+        price: '1x de R$ 157,00',
+    },
+]
---
+++ api/index.js
@@ -0,0 +1,16 @@
+import { moveisUsados, consertoDeRoupas, confeitaria, outrosServiços  } from "./dados.js";
+
+export default function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');  
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');  
+
+    const dadosDoCatalogo = {
+        moveisUsados,
+        consertoDeRoupas,
+        confeitaria,
+        outrosServiços
+    }
+
+    res.status(200).json(dadosDoCatalogo);
+}
