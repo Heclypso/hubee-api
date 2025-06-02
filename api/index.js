c268038ac4d2e3c024284427110304d3d729de26
Heclypso <heclypso@gmail.com>
2025-06-02 17:15:05 -0300

    #1 fix: corrigido url do cors

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import { moveisUsados, consertoDeRoupas, confeitaria, outrosServiços  } from "./dados.js";
 
 export default function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173/');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');  
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');  
 
